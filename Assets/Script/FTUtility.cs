03aff3168e3b066872c2525caccd80e20bd7d8dc
takumi hujishima <fujishima.t425@gmail.com>
2020-10-08 10:43:45 +0900

    createClonefunction

--- Assets/Script/FTUtility.cs
+++ Assets/Script/FTUtility.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FTUtility : MonoBehaviour
+public static class FTUtility
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameObject CloneObject(GameObject original)
     {
-        
+        GameObject go = MonoBehaviour.Instantiate(original);
+        go.transform.position = original.transform.position;
+        go.transform.localScale = original.transform.localScale;
+        go.transform.rotation = original.transform.rotation;
+
+        return go;
+    }
+    public static GameObject CloneObject(GameObject original,Vector2 clonePos)
+    {
+        GameObject go = MonoBehaviour.Instantiate(original);
+        go.transform.position = clonePos;
+        go.transform.localScale = original.transform.localScale;
+        go.transform.rotation = original.transform.rotation;
+
+        return go;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static GameObject CloneObjectInChild(GameObject original,Transform pearent)
     {
-        
+        GameObject go = MonoBehaviour.Instantiate(original);
+        go.transform.SetParent(pearent);
+        go.transform.localPosition = original.transform.position;
+        go.transform.localScale = original.transform.localScale;
+        go.transform.localRotation = original.transform.rotation;
+
+        return go;
     }
+
+    public static GameObject CloneObjectInChild(GameObject original, Transform pearent,Vector2 clonePos)
+    {
+        GameObject go = MonoBehaviour.Instantiate(original);
+        go.transform.SetParent(pearent);
+        go.transform.position = clonePos;
+        go.transform.localScale = original.transform.localScale;
+        go.transform.rotation = original.transform.rotation;
+
+        return go;
+    }
+
+
+
 }
